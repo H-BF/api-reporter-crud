c291a143de10b43704b48e8604e8fd4020fdb0f7
kiver.nikita <kiver.nikita@wb.ru>
2023-09-07 15:57:00 +0300

    added trust address, 404 and CORS  error handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser"
-import express, { Application } from "express"
+import cors from "cors"
+import express, { Application, Request, Response, NextFunction } from "express"
 import swaggerUi, { JsonObject } from 'swagger-ui-express';
 import { PrismaService } from "../database/prisma.service"
 import { ExceptionFilter } from "../errors/exception.filter"
@@ -26,14 +27,18 @@ import { variables } from "../common/var_storage/variables-storage";
 import { JsonSchemaRepository } from "../domain/json_schema/json-schema.repository";
 import { JsonSchemaService } from "../domain/json_schema/json-schema.service";
 import { JsonSchemaController } from "../domain/json_schema/json-schema.controller";
+import { CORSError } from "../errors/custom/cors-error";
+import { NotFoundError } from "../errors/custom/not-found-error";
 
 export class App {
 
     private app: Application
+    private path: string
 
     constructor() {
         this.app = express()
         this.app.use(bodyParser.json())
+        this.path = `/${variables.get("INGRESS_PATH")}/${variables.get("API_VERSION")}`
     }
 
     async start(): Promise<Application> {
@@ -59,27 +64,49 @@ export class App {
         const assertionSvc = new AssertionsService(assertionRepo)
         const jsonSchemaSvc = new JsonSchemaService(jsonSchemaRepo)
 
-        //Инициализируем и привязываем контроллеры
-        this.app.use('/api/v1', new LaunchController(launchSvc).router)
-        this.app.use('/api/v1', new LaunchErrorController(launchErrSvc).router)
-        this.app.use('/api/v1', new RequestController(reqSvc).router)
-        this.app.use('/api/v1', new ResponseController(resSvc).router)
-        this.app.use('/api/v1', new ExecutionsController(execSvc).router)
-        this.app.use('/api/v1', new AssertionsController(assertionSvc).router)
-        this.app.use('/api/v1', new JsonSchemaController(jsonSchemaSvc).router)
+        //Добавлям правила для CORS
+        this.app.use(cors({ origin: this.isTrustedAddress }))
 
-        //Биндим обработчик ошибок
-        const exf = new ExceptionFilter()
-        this.app.use(exf.catch.bind(exf))
+        //Инициализируем и привязываем контроллеры
+        this.app.use(this.path, new LaunchController(launchSvc).router)
+        this.app.use(this.path, new LaunchErrorController(launchErrSvc).router)
+        this.app.use(this.path, new RequestController(reqSvc).router)
+        this.app.use(this.path, new ResponseController(resSvc).router)
+        this.app.use(this.path, new ExecutionsController(execSvc).router)
+        this.app.use(this.path, new AssertionsController(assertionSvc).router)
+        this.app.use(this.path, new JsonSchemaController(jsonSchemaSvc).router)
 
         //Добавлям для DEV стейджа отображение swagger-документации
         if(variables.get("STAGE") === "dev") {
-            this.app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerTemplate({
+            this.app.use(`${this.path}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerTemplate({
                 host: variables.get("INGRESS_NAME"),
                 port: variables.get("INGRESS_PORT")
             }) as unknown as JsonObject))
         }
 
+        //Обработка несуществующих path
+        this.app.use(this.notFound)
+
+        //Биндим обработчик ошибок
+        const exf = new ExceptionFilter()
+        this.app.use(exf.catch.bind(exf))
+
         return this.app
+    }
+
+    private isTrustedAddress(
+        origin: string | undefined,
+        cb: (err: Error | null, allow?: boolean) => void
+    ) {
+        const whitelist = variables.get("TRUSTED_ADDRESS").split(";")
+        if (whitelist.indexOf(origin!) !== -1 || !origin) {
+            cb(null, true)
+        } else {
+            cb(new CORSError(origin));
+        }
+    }
+
+    private notFound(req: Request, res: Response, next: NextFunction) {
+        next(new NotFoundError())
     }
 }
